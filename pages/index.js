3699baae0deee1f8ac90143962c9dad9a90ca9e0
joseaugtop <joseaugustop.rocha@unesc.net>
2026-05-06 16:55:46 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Evelin, eu te amo muito, se tu também me ama, da uma risadinha! ❤️</h1>
+  return (
+    <h1>Evelin, eu te amo muito, se tu também me ama, da uma risadinha! ❤️</h1>
+  );
 }
 
-export default Home
+export default Home;
